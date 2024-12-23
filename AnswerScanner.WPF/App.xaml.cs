45a2e968ebd689f88a108087f67e7e1f6948443f
Vazovsk1y <anolrey.b@gmail.com>
2024-12-23 17:52:21 +0300

    -modified generic host initializing.

--- AnswerScanner.WPF/App.xaml.cs
+++ AnswerScanner.WPF/App.xaml.cs
@@ -3,31 +3,65 @@ using AnswerScanner.WPF.ViewModels;
 using AnswerScanner.WPF.Views.Windows;
 using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
+using AnswerScanner.WPF.Services;
+using AnswerScanner.WPF.Services.Interfaces;
+using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace AnswerScanner.WPF;
 
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
 public partial class App : Application
 {
-    public const string Title = "AnswerScanner";
+    public const string Name = "AnswerScanner";
 
-    public static readonly string Version;
-
+    public static string Version { get; } 
+    
     public static IServiceProvider Services { get; }
 
     static App()
     {
-        var host = Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
-        Services = host.Services;
-
         var version = Assembly.GetExecutingAssembly()
             .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
             .InformationalVersion;
 
-        ArgumentNullException.ThrowIfNull(version);
+        ArgumentException.ThrowIfNullOrWhiteSpace(version);
         Version = $"v{version}";
+        
+        var host = Host
+            .CreateDefaultBuilder(Environment.GetCommandLineArgs())
+            .ConfigureAppConfiguration((appConfig, _) =>
+            {
+                appConfig.HostingEnvironment.ApplicationName = Name;
+            })
+            .UseSerilog()
+            .ConfigureServices(ConfigureServices)
+            .Build();
+
+        Services = host.Services;
+    }
+    
+    private static void ConfigureServices(HostBuilderContext context, IServiceCollection collection)
+    {
+        collection.AddSingleton<MainWindow>();
+        collection.AddSingleton<MainWindowViewModel>();
+
+        collection.AddSingleton<IQuestionnaireParserFactory, QuestionnaireParserFactory>();
+        collection.AddTransient<PdfQuestionnaireParser>();
+        collection.AddTransient<SimpleImageQuestionnaireParser>();
+
+        collection.AddSingleton<IQuestionsExtractorFactory, QuestionsExtractorFactory>();
+        collection.AddTransient<YesNoAnswerOptionsQuestionsExtractor>();
+        collection.AddTransient<FiveAnswerOptionsQuestionsExtractor>();
+
+        collection.AddTransient<QuestionnairesUploadWindow>();
+        collection.AddTransient<QuestionnairesUploadViewModel>();
+        
+        collection.AddSingleton<IQuestionnaireFileExporterFactory, QuestionnaireFileExporterFactory>();
+        collection.AddTransient<QuestionnaireXlsxFileExporter>();
+
+        collection.AddTransient<QuestionnairesExportWindow>();
+        
+        collection.AddTransient<QuestionAddWindow>();
     }
 
     protected override void OnStartup(StartupEventArgs e)
